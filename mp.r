2b5c32bc7a976f5745e28c7464e5e56a2bac3a53
Chandler May <cjmay4754@gmail.com>
2016-02-25 13:27:27 -0500

    robust indexing in mp

--- mp.r
+++ mp.r
@@ -1,14 +1,29 @@
-#require(jsonlite)
+require(jsonlite)
 
 mp <- function(results.path, key.path) {
-    results <- read.csv(results.path)
-    #results.va <- fromJSON(results$var_arrays)
-    key <- read.delim(key.path)
+    results <- read.csv(results.path, header=T)
+    answer.col.idx <- which(grepl('^Answer.', colnames(results)))
+    if (length(answer.col.idx) != 1) {
+        stop('too many answers')
+    }
+    results.va <- lapply(as.list(results$Input.var_arrays),
+                         function(x) {fromJSON(as.character(x))})
 
-    ids <- (1:dim(results)[1])
-    d <- data.frame(ans.human=results[,1],
-                    ans.model=key$intruder.idx[ids],
-                    experiment=factor(key$experiment[ids]))
+    key <- read.delim(key.path, header=T)
+    key.ids <- vapply(1:dim(results)[1],
+                      function(i) {
+                          j <- which(key$wl.id ==
+                                     results.va[[i]]$wl_id)
+                          if (length(j) != 1) {
+                              stop('too many id matches')
+                          }
+                          j
+                      },
+                      42)
+
+    d <- data.frame(ans.human=results[,answer.col.idx],
+                    ans.model=key$intruder.idx[key.ids],
+                    experiment=factor(key$experiment[key.ids]))
     d$correct <- d$ans.human == d$ans.model
     d$total <- rep(1, dim(d)[1])
 
@@ -22,4 +37,4 @@ mp <- function(results.path, key.path) {
 
 results.path <- 'wiki-ru-dll.big.results.nocr.csv'
 key.path <- 'wiki-ru-dll-lda-trunc50-big.key.tsv'
-head(mp(results.path, key.path))
+print(mp(results.path, key.path))
