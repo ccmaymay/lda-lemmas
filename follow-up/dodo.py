d69742191dfba316feb5040586a11b610bbb5686
Chandler May <chandler.camille.may@gmail.com>
2021-10-23 12:29:31 -0400

    Use dynamic determination of languages.

--- follow-up/dodo.py
+++ follow-up/dodo.py
@@ -3,13 +3,19 @@ from pathlib import Path
 from follow_up.conversion import convert_polyglot_to_mallet
 
 DATA_DIR = '/export/b01/cmay14/polyglot'
-LANGUAGES = ('en', 'fa', 'ko', 'ru')
 MAX_NUM_DOCS = 200000
+
+
+def get_languages():
+    data_dir_path = Path(DATA_DIR)
+    for input_tar_path in data_dir_path.glob('??_wiki_text.tar.lzma'):
+        lang = input_tar_path.name[:2]
+        yield lang
 
 
 def task_untar():
     data_dir_path = Path(DATA_DIR)
-    for lang in LANGUAGES:
+    for lang in get_languages():
         input_tar_path = data_dir_path / f'{lang}_wiki_text.tar.lzma'
         output_text_path = data_dir_path / lang / 'full.txt'
         yield {
@@ -22,7 +28,7 @@ def task_untar():
 
 def task_convert_polyglot_to_mallet():
     data_dir_path = Path(DATA_DIR)
-    for lang in LANGUAGES:
+    for lang in get_languages():
         input_path = data_dir_path / lang / 'full.txt'
         output_path = data_dir_path / lang / 'mallet.txt'
         yield {
@@ -39,7 +45,7 @@ def task_convert_polyglot_to_mallet():
 
 def task_subsample():
     data_dir_path = Path(DATA_DIR)
-    for lang in LANGUAGES:
+    for lang in get_languages():
         input_path = data_dir_path / lang / 'mallet.txt'
         output_path = data_dir_path / lang / 'mallet-sub.txt'
         yield {
