ec549fc84266ebdecc5bc4bed367d2b29128b26c
Chandler May <chandler.camille.may@gmail.com>
2021-10-30 02:59:07 -0400

    Subsample before lemmatize.

--- follow-up/dodo.py
+++ follow-up/dodo.py
@@ -1,9 +1,8 @@
-from functools import partial
 from pathlib import Path
 
 import pycountry  # type: ignore
 
-from follow_up.util import subsample_parallel
+from follow_up.util import subsample
 from follow_up.conversion import convert_polyglot_to_mallet
 from follow_up.lemmatization import parse_treetagger, lemmatize_polyglot
 
@@ -22,13 +21,14 @@ LANGUAGE_NAMES = dict(
 def task_untar():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
-        input_tar_path = data_root_path / f'{lang}_wiki_text.tar.lzma'
-        output_text_path = data_root_path / lang / 'full.txt'
+        input_path = data_root_path / f'{lang}_wiki_text.tar.lzma'
+        output_path = data_root_path / lang / 'full.txt'
         yield {
             'name': lang,
-            'file_dep': [input_tar_path],
-            'actions': [f'tar -C {DATA_ROOT} -xvJf %(dependencies)s'],
-            'targets': [output_text_path],
+            # consider up-to-date as long as target files exist
+            'uptodate': [True],
+            'actions': [f'tar -C {DATA_ROOT} -xvJf {input_path}'],
+            'targets': [output_path],
         }
 
 
@@ -36,15 +36,23 @@ def task_to_mallet():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
         input_paths = [
-            data_root_path / lang / 'full.txt',
-            data_root_path / lang / 'lem-polyglot.txt',
-            data_root_path / lang / 'lem-treetagger.txt',
+            data_root_path / lang / 'sub.txt',
+            data_root_path / lang / 'sub.lem-polyglot.txt',
+            data_root_path / lang / 'sub.lem-treetagger.parsed.txt',
         ]
-        for input_path in input_paths:
+        prev_tasks = [
+            f'subsample:{lang}',
+            f'lemmatize_polyglot:{lang}',
+            f'parse_treetagger:{lang}',
+        ]
+        for (input_path, prev_task) in zip(input_paths, prev_tasks):
             output_path = input_path.with_suffix('.mallet.txt')
             yield {
                 'name': f'{lang}-{input_path.stem}',
-                'file_dep': [input_path],
+                # consider up-to-date as long as target files exist
+                'uptodate': [True],
+                # ensure other task happens first without affecting uptodateness
+                'task_dep': [prev_task],
                 'actions': [(convert_polyglot_to_mallet, (), dict(
                     lang=lang,
                     input_path=input_path,
@@ -54,20 +62,23 @@ def task_to_mallet():
             }
 
 
-def task_subsample_mallet():
+def task_subsample():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
-        input_paths = [
-            data_root_path / lang / 'full.mallet.txt',
-            data_root_path / lang / 'lem-polyglot.mallet.txt',
-            data_root_path / lang / 'lem-treetagger.mallet.txt',
-        ]
-        output_paths = [input_path.with_suffix('.sub.txt') for input_path in input_paths]
+        input_path = data_root_path / lang / 'full.txt'
+        output_path = data_root_path / lang / 'sub.txt'
         yield {
             'name': lang,
-            'file_dep': input_paths,
-            'actions': [partial(subsample_parallel, max_num_lines=MAX_NUM_DOCS)],
-            'targets': output_paths,
+            # consider up-to-date as long as target files exist
+            'uptodate': [True],
+            # ensure untar happens first without affecting uptodateness
+            'task_dep': [f'untar:{lang}'],
+            'actions': [(subsample, (), dict(
+                input_path=input_path,
+                output_path=output_path,
+                max_num_docs=MAX_NUM_DOCS,
+            ))],
+            'targets': [output_path],
         }
 
 
@@ -75,13 +86,16 @@ def task_lemmatize_treetagger():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
         lang_name = LANGUAGE_NAMES[lang]
-        input_path = data_root_path / lang / 'full.txt'
-        output_path = data_root_path / lang / 'lem-treetagger-unparsed.txt'
+        input_path = data_root_path / lang / 'sub.txt'
+        output_path = input_path.with_suffix('.lem-treetagger.txt')
         yield {
             'name': lang,
-            'file_dep': [input_path],
+            # consider up-to-date as long as target files exist
+            'uptodate': [True],
+            # ensure subsample happens first without affecting uptodateness
+            'task_dep': [f'subsample:{lang}'],
             'actions': [
-                f'treetagger/cmd/tree-tagger-{lang_name} < %(dependencies)s > %(targets)s'
+                f'treetagger/cmd/tree-tagger-{lang_name} < {input_path} > {output_path}'
             ],
             'targets': [output_path],
         }
@@ -90,11 +104,14 @@ def task_lemmatize_treetagger():
 def task_parse_treetagger():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
-        input_path = data_root_path / lang / 'lem-treetagger-unparsed.txt'
-        output_path = data_root_path / lang / 'lem-treetagger.txt'
+        input_path = data_root_path / lang / 'sub.lem-treetagger.txt'
+        output_path = input_path.with_suffix('.parsed.txt')
         yield {
             'name': lang,
-            'file_dep': [input_path],
+            # consider up-to-date as long as target files exist
+            'uptodate': [True],
+            # ensure lemmatize happens first without affecting uptodateness
+            'task_dep': [f'lemmatize_treetagger:{lang}'],
             'actions': [(parse_treetagger, (), dict(
                 lang=lang,
                 input_path=input_path,
@@ -107,14 +124,14 @@ def task_parse_treetagger():
 def task_lemmatize_polyglot():
     data_root_path = Path(DATA_ROOT)
     for lang in LANGUAGES:
-        input_path = data_root_path / lang / 'full.txt'
-        output_path = data_root_path / lang / 'lem-polyglot.txt'
+        input_path = data_root_path / lang / 'sub.txt'
+        output_path = input_path.with_suffix('.lem-polyglot.txt')
         yield {
             'name': lang,
             # consider up-to-date as long as target files exist
             'uptodate': [True],
-            # ensure untar happens first without affecting uptodateness
-            'task_dep': [f'untar:{lang}'],
+            # ensure subsample happens first without affecting uptodateness
+            'task_dep': [f'subsample:{lang}'],
             'actions': [(lemmatize_polyglot, (), dict(
                 lang=lang,
                 input_path=input_path,
