d74bcfdc716fcb5eed2e456123ec00f96792d9cc
Chandler May <chandler.camille.may@gmail.com>
2021-10-29 20:41:37 -0400

    Simplify doit config, expand to-mallet task.

--- follow-up/dodo.py
+++ follow-up/dodo.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Iterable
 
 import pycountry  # type: ignore
 
@@ -10,6 +9,7 @@ DATA_ROOT = 'polyglot'
 TREETAGGER_ROOT = 'treetagger'
 MAX_NUM_DOCS = 200000
 
+LANGUAGES = ('en', 'fa', 'ko', 'ru')
 LANGUAGE_NAMES = dict(
     (lang.alpha_2, lang.name.lower())
     for lang in pycountry.languages
@@ -17,18 +17,11 @@ LANGUAGE_NAMES = dict(
 )
 
 
-def get_languages() -> Iterable[str]:
-    data_dir_path = Path(DATA_ROOT)
-    for input_tar_path in data_dir_path.glob('??_wiki_text.tar.lzma'):
-        lang = input_tar_path.name[:2]
-        yield lang
-
-
 def task_untar():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
-        input_tar_path = data_dir_path / f'{lang}_wiki_text.tar.lzma'
-        output_text_path = data_dir_path / lang / 'full.txt'
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
+        input_tar_path = data_root_path / f'{lang}_wiki_text.tar.lzma'
+        output_text_path = data_root_path / lang / 'full.txt'
         yield {
             'name': lang,
             'file_dep': [input_tar_path],
@@ -37,28 +30,33 @@ def task_untar():
         }
 
 
-def task_convert_polyglot_to_mallet():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
-        input_path = data_dir_path / lang / 'full.txt'
-        output_path = data_dir_path / lang / 'mallet.txt'
-        yield {
-            'name': lang,
-            'file_dep': [input_path],
-            'actions': [(convert_polyglot_to_mallet, (), dict(
-                lang=lang,
-                input_path=input_path,
-                output_path=output_path
-            ))],
-            'targets': [output_path],
-        }
+def task_to_mallet():
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
+        input_paths = [
+            data_root_path / lang / 'full.txt',
+            data_root_path / lang / 'lem-polyglot.txt',
+            data_root_path / lang / 'lem-treetagger.txt',
+        ]
+        for input_path in input_paths:
+            output_path = input_path.with_suffix('.mallet.txt')
+            yield {
+                'name': f'{lang}-{input_path.stem}',
+                'file_dep': [input_path],
+                'actions': [(convert_polyglot_to_mallet, (), dict(
+                    lang=lang,
+                    input_path=input_path,
+                    output_path=output_path
+                ))],
+                'targets': [output_path],
+            }
 
 
-def task_subsample():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
-        input_path = data_dir_path / lang / 'mallet.txt'
-        output_path = data_dir_path / lang / 'mallet-sub.txt'
+def task_subsample_mallet():
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
+        input_path = data_root_path / lang / 'mallet.txt'
+        output_path = data_root_path / lang / 'mallet-sub.txt'
         yield {
             'name': lang,
             'file_dep': [input_path],
@@ -68,11 +66,11 @@ def task_subsample():
 
 
 def task_lemmatize_treetagger():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
         lang_name = LANGUAGE_NAMES[lang]
-        input_path = data_dir_path / lang / 'full.txt'
-        output_path = data_dir_path / lang / 'lem-treetagger.txt'
+        input_path = data_root_path / lang / 'full.txt'
+        output_path = data_root_path / lang / 'lem-treetagger-unparsed.txt'
         yield {
             'name': lang,
             'file_dep': [input_path],
@@ -84,10 +82,10 @@ def task_lemmatize_treetagger():
 
 
 def task_parse_treetagger():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
-        input_path = data_dir_path / lang / 'lem-treetagger.txt'
-        output_path = data_dir_path / lang / 'lem-treetagger-parsed.txt'
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
+        input_path = data_root_path / lang / 'lem-treetagger-unparsed.txt'
+        output_path = data_root_path / lang / 'lem-treetagger.txt'
         yield {
             'name': lang,
             'file_dep': [input_path],
@@ -101,10 +99,10 @@ def task_parse_treetagger():
 
 
 def task_lemmatize_polyglot():
-    data_dir_path = Path(DATA_ROOT)
-    for lang in get_languages():
-        input_path = data_dir_path / lang / 'full.txt'
-        output_path = data_dir_path / lang / 'lem-polyglot.txt'
+    data_root_path = Path(DATA_ROOT)
+    for lang in LANGUAGES:
+        input_path = data_root_path / lang / 'full.txt'
+        output_path = data_root_path / lang / 'lem-polyglot.txt'
         yield {
             'name': lang,
             # consider up-to-date as long as target files exist
