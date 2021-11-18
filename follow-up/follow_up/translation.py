bcc9716202655a109e1f95c3b04310d8533b044f
Chandler May <chandler.camille.may@gmail.com>
2021-11-18 00:13:47 -0500

    Add metadata to translate output.

--- follow-up/follow_up/translation.py
+++ follow-up/follow_up/translation.py
@@ -1,11 +1,27 @@
 import requests
 import uuid
-from typing import List
+from typing import List, NamedTuple
 
 MAX_NUM_LOOKUP_WORDS = 10
 
 
-def translate(words: List[str], from_lang: str, to_lang: str, subscription_key: str) -> List[str]:
+class BackTranslation(NamedTuple):
+    source: str
+    frequency: int
+
+
+class WordTranslation(NamedTuple):
+    source: str
+    target: str
+    confidence: float
+    back_translations: List[BackTranslation]
+
+
+def translate(
+        words: List[str],
+        from_lang: str,
+        to_lang: str,
+        subscription_key: str) -> List[List[WordTranslation]]:
     if any(len(word) > 100 for word in words):
         raise Exception('Words must be at most 100 characters each')
 
@@ -16,7 +32,7 @@ def translate(words: List[str], from_lang: str, to_lang: str, subscription_key: 
         'to': to_lang,
     }
 
-    translated_words = []
+    word_translations = []
     for start_word_idx in range(0, len(words), MAX_NUM_LOOKUP_WORDS):
         headers = {
             'Ocp-Apim-Subscription-Key': subscription_key,
@@ -24,12 +40,34 @@ def translate(words: List[str], from_lang: str, to_lang: str, subscription_key: 
             'Content-type': 'application/json',
             'X-ClientTraceId': str(uuid.uuid4())
         }
-        body = [
-            {'text': word}
-            for word in words[start_word_idx:start_word_idx + MAX_NUM_LOOKUP_WORDS]
-        ]
+        source_words = words[start_word_idx:start_word_idx + MAX_NUM_LOOKUP_WORDS]
+        body = [{'text': word} for word in source_words]
         request = requests.post(url, params=params, headers=headers, json=body)
         response = request.json()
-        translated_words += [item['translations'][0]['normalizedTarget'] for item in response]
+        if isinstance(response, list):
+            word_translations += [
+                [
+                    WordTranslation(
+                        source=item['normalizedSource'],
+                        target=translation['normalizedTarget'],
+                        confidence=translation['confidence'],
+                        back_translations=[
+                            BackTranslation(
+                                source=bt['normalizedText'],
+                                frequency=bt['frequencyCount']
+                            )
+                            for bt in translation['backTranslations']
+                        ],
+                    )
+                    for translation in item['translations']
+                ]
+                for item in response
+            ]
+        elif isinstance(response, dict) and 'error' in response:
+            error_message = response['error']['message']
+            error_code = response['error']['code']
+            raise Exception(f'Received API error code {error_code}: {error_message}')
+        else:
+            raise Exception(f'Received unrecognized API response {response}')
 
-    return translated_words
+    return word_translations
